2b1ca3e69dff3f20259d19df50e0395f1c19273a
joe witcpalek <joe.witcpalek@gmail.com>
2019-08-05 20:51:04 -0400

    more updates

--- src/main/cpp/subsys/IMechanism.h
+++ src/main/cpp/subsys/IMechanism.h
@@ -1,17 +1,52 @@
-/*
- * IMechanism.h
- */
+
+///====================================================================================================================================================
+/// Copyright 2019 Lake Orion Robobitcs FIRST Team 302
+///
+/// Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"),
+/// to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense,
+/// and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+///
+/// The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+///
+/// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+/// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+/// DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE
+/// OR OTHER DEALINGS IN THE SOFTWARE.
+///====================================================================================================================================================
 
 #pragma once
 
+///========================================================================================================
+/// IMechanism.h
+///========================================================================================================
+///
+/// File Description:
+///     This is the interface for all subsystems
+///
+///========================================================================================================
 
+// C++ Includes
 #include <vector>
-#include <xmlhw/PIDData.h>
 
+// FRC includes
+
+// Team 302 includes
+#include <xmlhw/PIDData.h>
+// Third Party Includes
+
+
+///========================================================================================================
+///	 Class:			IMechanism
+///  Description:	Interface for subsystems
+///========================================================================================================
 class IMechanism
 {
 	public:
 
+        ///==================================================================================
+        /// enum:           MECHANISM_TYPE
+        /// description:    Indicates the type of mechanism
+        ///==================================================================================
         enum MECHANISM_TYPE
         {
             UNKNOWN_MECHANISM = -1,
@@ -23,6 +58,7 @@ class IMechanism
             MAX_MECHANISM_TYPES
         };
 
+        // todo:  move this
         enum MECHANISM_PARAM_TYPE
         {
             MECHANISM_PARAM_UNKNOWN = -1,
@@ -69,15 +105,54 @@ class IMechanism
             MAX_MECHANISM_PARAM_TYPES
         };
 
-
-
-
+        ///==================================================================================
+        /// method:         GetType
+        /// description:    Indicates the type of mechanism this is
+        /// returns:        MECHANISM_TYPE
+        ///==================================================================================
         virtual MECHANISM_TYPE GetType() const = 0;
+
+
+        ///==================================================================================
+        /// method:         Set
+        /// description:    Run mechanism in open loop (percent output)
+        /// returns:        void
+        ///==================================================================================
+        virtual void Set
+        (
+            double      value       /// <I> - percent output for the motor(s)
+        ) = 0;
+
+        ///==================================================================================
+        /// method:         SetPosition
+        /// description:    Run mechanism in closed loop position mode.  The value is in 
+        ///                 inches or degrees.
+        /// returns:        void
+        ///==================================================================================
+        virtual void SetPosition 
+        (
+            double      pos       /// <I> - target position (inches or degrees)
+        ) = 0;
+
+        ///==================================================================================
+        /// method:         SetSpeed
+        /// description:    Run mechanism in closed loop velocity mode.  The value is in 
+        ///                 inches per second or degrees per second.
+        /// returns:        void
+        ///==================================================================================
+        virtual void SetSpeed 
+        (
+            double      speed       /// <I> - target speed (inches / second or degrees / second)
+        ) = 0;
+
+        // todo:  move
         virtual void SetParam
         (
             IMechanism::MECHANISM_PARAM_TYPE    param,          // <I> - parameter to set
             double                              value           // <I> - parameter value
         ) = 0;
+
+        // todo: move
         virtual void SetPID
         (
             PIDData*        pid                 // <I> - PID control information
